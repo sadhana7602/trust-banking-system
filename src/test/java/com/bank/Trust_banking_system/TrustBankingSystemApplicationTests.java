bc3fe0fc42919691aa06c9b43c2119cecf9599cd
sadhana7602 <sadhana070602@gmail.com>
2026-02-23 13:14:52 +0000

    Initial commit - Trust Banking System backend

---
+++ src/test/java/com/bank/Trust_banking_system/TrustBankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.Trust_banking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrustBankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
